20fb817eb5b2ef7277aaffad2b208424e0b04140
CodeKrakken <newholm1@hotmail.com>
2021-03-23 13:20:03 +0000

    Add chart js

--- script.js
+++ script.js
@@ -1,14 +1,48 @@
+const ctx = document.getElementById('chart').getContext('2d');
+const chart = new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: ['red', 'green', 'blue'],
+    datasets: [{
+      label: '# of Votes',
+      data: [12, 19, 3],
+      backgroundColor: [
+        'red',
+        'green',
+        'blue'
+      ],
+      borderColor: [
+        'red',
+        'green',
+        'blue'
+      ],
+      borderWidth: 1
+    }]
+  },
+  options: {
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: true
+        }
+      }]
+    }
+  }
+});
+
+
 console.time();
 
 
 
 // Algorithm to time goes here
-let counter = 0
-for (i = 0; i < 10000; i++) {
-  counter ++
+let array = []
+for(let i = 0; i < 10000; i++) {
+  array.push(i)
 }
+console.log(array.length)
 
 
 
 
-console.timeEnd(); 
+console.timeEnd();
