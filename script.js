4fbdd6863a013307697a6e0c7d7b6fe29d04e774
CodeKrakken <newholm1@hotmail.com>
2021-03-24 17:59:42 +0000

    Set code to run full set of tests at once, increasing array size automatically

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 let array = []
 let sampleSize = 10
-for(let i = 0; i < sampleSize; i++) {
-  array.push(i)
-}
 
-for(let i = 0; i < 10; i++) {
-  console.time();
-  console.log(array[array.length-1])
-  console.timeEnd()
-  console.log(array.length)
+for(let h = 0; sampleSize < 1500000; h++) {
+
+  for(let i = 0; i < sampleSize; i++) {
+    array.push(i)
+  }
+
+  for(let j = 0; j < 10; j++) {
+    console.time();
+    console.log(array[array.length-1])
+    console.timeEnd()
+  }
+
+  sampleSize = sampleSize * 2
 }
