73c97f5bf3af3d2ea1d0fd8ea73d2e9decb47b12
CodeKrakken <newholm1@hotmail.com>
2021-03-26 10:46:39 +0000

    Expand tests for no duplicates returned

--- spec/DuplicateFinderSpec.js
+++ spec/DuplicateFinderSpec.js
@@ -7,5 +7,13 @@ describe('duplicate finder', function() {
 
   it('returns an empty array when there are no duplicates', function() {
     expect(duplicateFinder.findDuplicates(['Anna'])).toEqual([])
+    expect(duplicateFinder.findDuplicates(['Reg'])).toEqual([])
+    expect(duplicateFinder.findDuplicates(['Anna', 'Reg'])).toEqual([])
   })
+
+  it('returns an array of a single name if given an array of two matching names', function() {
+    expect(duplicateFinder.findDuplicates(['Reg', 'Reg'])).toEqual(['Reg'])
+  })
+
+
 })
